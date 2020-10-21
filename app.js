7bddf0fcd81c0a61e9049a69af4582ed32ac8e50
felix rodriguezmanzaneque <felixrmj@gmail.com>
2020-10-21 20:17:22 +0200

    Added filterTasks function

--- app.js
+++ app.js
@@ -103,5 +103,12 @@ function clearTasks(e){
 function filterTasks(e) {
   const text = e.target.value.toLowerCase();
 
-  console.log(text);
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display='none';
+    }
+  });
 }
