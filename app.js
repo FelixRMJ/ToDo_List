74d9f5573433c03a5c619bc9b261647871882896
felix rodriguezmanzaneque <felixrmj@gmail.com>
2020-10-21 20:06:51 +0200

    Added Clear tasks function and working on filter tasks function

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
+
 // INDEX
 
 //1.- DEFINE UI VARS
 //2.- LOAD ALL EVENT LISTENERS
 //  2.1.- ADD TASK EVENT
 //  2.2.- REMOVE TASK EVENT
+//  2.3.- CLEAR TASKS EVENT
+//  2.4.- FILTER TASKS EVENT
+
 //3.- ADD TASK
-//  3.1.-CREATE LI ELEMENT
+//  3.1.- CREATE LI ELEMENT
 //    3.1.1- ADD CLASS TO LI ELEMENT
 //    3.1.2.- CREATE TEXT NODE AND APPEND TO LI ELEMENT
-//  3.2.-CREATE LINK ELEMENT
-//    3.2.1-ADD CLASS TO THE LINK ELEMENT
-//    3.2.2.-ADD ICON HTML
+//  3.2.- CREATE LINK ELEMENT
+//    3.2.1- ADD CLASS TO THE LINK ELEMENT
+//    3.2.2.- ADD ICON HTML
 //    3.3.3.- APPEND THE LINK TO LI
 //  3.3.- APPEND THE LI TO THE UL
 //  3.4.- CLEAR INPUT
+
 //4.- REMOVE TASK
+
+//5.- CLEAR TASKS
+//  5.1.- METHOD INNERHTML (NOT THE MOST RECOMMENDED)
+//  5.1.- METHOD REMOVECHILD (FASTER BUT NOT TOO MUCH)
+//6.- FILTER TASKS
 
 // Define UI Vars
 const form = document.querySelector('#task-form');
@@ -32,6 +42,10 @@ function loadEventListeners() {
   form.addEventListener('submit', addTask);
   //remove task event
   taskList.addEventListener('click', removeTask);
+  // Clear tasks event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -67,6 +81,27 @@ function addTask(e) {
 // Remove Task
 function removeTask(e) {
  if(e.target.parentElement.classList.contains('delete-item')) {
-   e.target.parentElement.parentElement.remove();
+   if(confirm('Are you sure?')){
+    e.target.parentElement.parentElement.remove();
+   }
  }
+}
+
+// Clear tasks
+function clearTasks(e){
+  // One option
+  // taskList.innerHTML = '';
+
+  //Faster option but not too much faster
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+
+  }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  console.log(text);
 }
